5a0e300ce5e35c8dca6b89d3029911cdd297b0ff
DOMBALD Steeve <66547680+DombaldSteeve@users.noreply.github.com>
2022-05-05 15:57:52 +0200

    initialisation projet

---
+++ src/main/java/com/springback/etsyegna/EtsyegnaApplication.java
@@ -0,0 +1,13 @@
+package com.springback.etsyegna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtsyegnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtsyegnaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springback/etsyegna/EtsyegnaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springback.etsyegna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtsyegnaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
